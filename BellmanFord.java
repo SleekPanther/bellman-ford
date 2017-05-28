db17b1308f431097064a7efc939abd06e0bdad62
SleekPanther <macrobyte2@gmail.com>
2017-05-28 14:41:48 -0400

    printShortestPaths() works & cleanup

--- BellmanFord.java
+++ BellmanFord.java
@@ -9,32 +9,30 @@ public class BellmanFord {
 	int goalVertex;
 	
 	public void findShortestPaths(int[][][] inputGraph, int inputStartVertex, int inputGoalVertex){
-		startVertex = inputStartVertex;
-		goalVertex = inputGoalVertex;
-		graph=inputGraph;
+		this.startVertex = inputStartVertex;
+		this.goalVertex = inputGoalVertex;
+		this.graph=inputGraph;
 		int vertexCount = graph.length;
 
 		memoTable = new int[vertexCount][vertexCount];
 		successors = new int[vertexCount][vertexCount];
 		for(int vertex=0; vertex<vertexCount; vertex++){
 			memoTable[0][vertex] = Integer.MAX_VALUE;	//Most nodes cannot reach the goalVertex using 0 edges, so set it to "infinity"
-			successors[0][vertex] = -1;		//initialize with no successor
+			successors[0][vertex] = -1;		//initialize 1st row with no successor
 		}
 		memoTable[0][goalVertex] = 0;	//shortest path from Goal Vertex to itself uses 0 edges
-		successors[0][goalVertex] = goalVertex;	//goalVertex is a path of cost 0 starting & ending at itself
+		successors[0][goalVertex] = goalVertex;	//path from goalVertex to goalVertex is possible with 0 edges so it's its own successor
 
 		for(int i=1; i<vertexCount; i++){		//shortest path uses up to n-1 edges in a graph with n nodes
 			for(int sourceVertex=0; sourceVertex<vertexCount; sourceVertex++){
-				ArrayList<ArrayList<Integer>> possiblePathCosts = new ArrayList<ArrayList<Integer>>();	//Holds costs for all possible paths, smallest value is chosen
-				possiblePathCosts.add(new ArrayList<Integer>(Arrays.asList(memoTable[i-1][sourceVertex], successors[i-1][sourceVertex])) );	//inherit value from 1 less edge
+				ArrayList<ArrayList<Integer>> possiblePathCosts = new ArrayList<ArrayList<Integer>>();	//Holds costs for all possible paths & the successor for each of those paths
+				possiblePathCosts.add(new ArrayList<Integer>(Arrays.asList(memoTable[i-1][sourceVertex], successors[i-1][sourceVertex])) );		//inherit value from 1 less edge
 				for(int k=0; k<graph[sourceVertex].length; k++){	//loop over all edges incident to sourceVertex
 					int destinationVertex = graph[sourceVertex][k][0];
 					int weight = graph[sourceVertex][k][1];
 					int newCost = Integer.MAX_VALUE;	//initialize to infinity to avoid overflow
-					int newSuccessor = -10;		//initialize to no successor
 					if(memoTable[i-1][destinationVertex] != Integer.MAX_VALUE){		//if the value from previous row is NOT infinity
-						newCost = weight + memoTable[i-1][destinationVertex];	//calculate new cost from optimal substurcture (avoiding overflow)
-						newSuccessor = destinationVertex;
+						newCost = weight + memoTable[i-1][destinationVertex];	//calculate new cost from optimal substructure (avoiding overflow)
 					}
 					possiblePathCosts.add(new ArrayList<Integer>(Arrays.asList(newCost, destinationVertex)));
 				}
@@ -42,7 +40,7 @@ public class BellmanFord {
 				int minCost = possiblePathCosts.get(0).get(0);
 				int successor = possiblePathCosts.get(0).get(1);
 				for(int j=0; j<possiblePathCosts.size(); j++){
-					if(possiblePathCosts.get(j).get(0) < minCost){
+					if(possiblePathCosts.get(j).get(0) < minCost){	//if we find a smaller value, update minCost & successor where that minCost was found
 						minCost=possiblePathCosts.get(j).get(0);
 						successor=possiblePathCosts.get(j).get(1);
 					}
@@ -51,6 +49,7 @@ public class BellmanFord {
 				successors[i][sourceVertex]=successor;
 			}
 		}
+		System.out.println("\tMemoization Table");
 		printTable();
 		System.out.println();
 		printShortestPaths();
@@ -77,25 +76,40 @@ public class BellmanFord {
 		}
 	}
 
-	//Requires memoization talbe to be filled
+	//Requires memoization table & successors to be filled
 	private void printShortestPaths(){
-		for(int i=0; i<successors.length; i++){
-			for(int j=0; j<successors[i].length; j++){
-				System.out.print(successors[i][j]+"\t");
+		//Find shortest paths for from all vertices to the goalVertex
+		for(int vertex=0; vertex<graph.length; vertex++){
+			System.out.print("Shortest path from "+vertexNames[vertex] + " to " +vertexNames[goalVertex] + ":  ");
+			int nextVertex = successors[successors.length -1][vertex];	//initial starting vertex in path, updated until goal vertex is reached
+			if(nextVertex == -1){	// -1 represents no successor, so no path exists
+				System.out.println("No path exists");
 			}
-			System.out.println();
+			else{	//Else actually retrace the path
+				System.out.print(vertexNames[vertex]);		//print start of the path
+				for(int row = successors.length -1; row>=0; row--){		//start from the bottom of successors table decreaseing row each iteration as 1 edge is used up
+					if(nextVertex == goalVertex){		//Reached the end condition
+						System.out.println("-->"+vertexNames[goalVertex] + "\t(Cost = "+memoTable[memoTable.length-1][vertex]+")");
+						break;
+					}
+					else{
+						System.out.print("-->"+vertexNames[nextVertex]);		//print the next vertex found
+						nextVertex = successors[row-1][nextVertex];		//find next vertex in path
+					}
+				}
+			}
 		}
 	}
 	
 	
 	public static void main(String[] args) {
 		BellmanFord pathFinder = new BellmanFord();
-		int[][][] inputGraph = { { {1, 6}, {2, 7} },
-							{ {3, 5}, {4, -4}, {2, 8} },
-							{ {3, -3}, {4, 9} },
-							{ {1, -2} },
-							{ {0, 2}, {3, 7} }
-							};
+		int[][][] inputGraph = {{ {1, 6}, {2, 7} },
+								{ {3, 5}, {4, -4}, {2, 8} },
+								{ {3, -3}, {4, 9} },
+								{ {1, -2} },
+								{ {0, 2}, {3, 7} }
+								};
 		int startingVertex =0;
 		int goalVertex = 4;
 		pathFinder.findShortestPaths(inputGraph, startingVertex, goalVertex);
