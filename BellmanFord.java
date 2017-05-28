6f93e0a17eaf6f031a92d4448cf275f32e8b2b33
SleekPanther <macrobyte2@gmail.com>
2017-05-28 12:36:22 -0400

    WORKS! (most basic version)

--- BellmanFord.java
+++ BellmanFord.java
@@ -4,7 +4,7 @@ public class BellmanFord {
 	private int[][] memoTable;
 	private String[] vertexNames = {"S", "B", "C", "D", "T"};
 	
-	public void findShortestPaths(int[][][] graph, int startingvertex, int goalVertex){
+	public void findShortestPaths(int[][][] graph, int startingVertex, int goalVertex){
 		int vertexCount = graph.length;
 		int edgeCount=0;
 		for(int i=0; i<vertexCount; i++){	//Loop over entire graph & count the number of edges
@@ -13,27 +13,33 @@ public class BellmanFord {
 			}
 		}
 
-		memoTable = new int[edgeCount][vertexCount];
+		memoTable = new int[vertexCount][vertexCount];
 		for(int vertex=0; vertex<vertexCount; vertex++){
 			memoTable[0][vertex] = Integer.MAX_VALUE;	//No path to a node with 0 edges, so set in "infinity"
 		}
 		memoTable[0][goalVertex] = 0;	//shortest path from Goal Vertex to itself uses 0 edges
 
-		// for(int i=1; i<edgeCount; i++){	//or edgeCount -1?
-		// 	for(int sourceVertex=0; sourceVertex<vertexCount; sourceVertex++){
-		// 		memoTable[i][sourceVertex]=memoTable[i-1][sourceVertex];	//inherit value from 1 less edge
+		for(int i=1; i<vertexCount; i++){		//shortest path uses up to n-1 edges in a graph with n nodes
+			for(int sourceVertex=0; sourceVertex<vertexCount; sourceVertex++){
+				memoTable[i][sourceVertex]=memoTable[i-1][sourceVertex];	//inherit value from 1 less edge
 
-		// 		ArrayList<Integer> possiblePathWeights = new ArrayList<Integer>();
-		// 		possiblePathWeights.add(memoTable[i][sourceVertex]);	//copy existing value
-		// 		for(int k=0; k<graph[i].length; k++){
-		// 			System.out.println("edge " + i "-->"+ graph[i][vertex][1] + "(weight="+graph[i][vertex][2]);
-		// 			// int destinationVertex = graph[i][vertex][1];
-		// 			// int weight = graph[i][k][2];
-		// 			// possiblePathWeights.add(weight + memoTable[i-1][destinationVertex]);
-		// 		}
-		// 		memoTable[i][sourceVertex]=Collections.min(possiblePathWeights);	//find min of all possible paths found
-		// 	}
-		// }
+				ArrayList<Integer> possiblePathWeights = new ArrayList<Integer>();
+				possiblePathWeights.add(memoTable[i][sourceVertex]);	//copy existing value
+				for(int k=0; k<graph[sourceVertex].length; k++){
+					int destinationVertex = graph[sourceVertex][k][0];
+					int weight = graph[sourceVertex][k][1];
+					System.out.println("edge " + sourceVertex +"-->"+ destinationVertex + " w="+weight + "  i="+i);
+					int newCost = Integer.MAX_VALUE;	//initialize to infinity
+					if(memoTable[i-1][destinationVertex] != Integer.MAX_VALUE){
+						newCost = weight + memoTable[i-1][destinationVertex];	//calculate new cost avoiding overflow
+					}
+					possiblePathWeights.add(newCost);
+					System.out.print("cw new "+newCost);
+				}
+				System.out.println(possiblePathWeights);
+				memoTable[i][sourceVertex]=Collections.min(possiblePathWeights);	//find min of all possible paths found
+			}
+		}
 
 		printTable();
 	}
@@ -57,9 +63,9 @@ public class BellmanFord {
 							{ {1, -2} },
 							{ {0, 2}, {3, 7} }
 							};
-		int startingvertex =0;
+		int startingVertex =0;
 		int goalVertex = 4;
-		pathFinder.findShortestPaths(inputGraph, startingvertex, goalVertex);
+		pathFinder.findShortestPaths(inputGraph, startingVertex, goalVertex);
 	}
 
 }
